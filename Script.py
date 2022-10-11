212e75004f8636609abee8ff31e97aa3756706ed
Inmyveinz <85662074+Inmyveinz@users.noreply.github.com>
2022-10-11 22:02:43 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -1,6 +1,6 @@
 class script(object):
-    START_TXT = """𝙷𝙴𝙻o {},
-𝙼𝚈 𝙽𝙰𝙼𝙴 𝙸𝚂 <a href=https://t.me/{}>{}</a> Iam from WlMl. i will be your tour guide whenever u visit WlMl."""
+    START_TXT = """Hello {},
+My Name Is <a href=https://t.me/{}>{}</a> Iam from WlMl. i will be your tour guide whenever u visit WlMl."""
     HELP_TXT = """𝙷𝙴𝚈 {}
 𝙷𝙴𝚁𝙴 𝙸𝚂 𝚃𝙷𝙴 𝙷𝙴𝙻𝙿 𝙵𝙾𝚁 𝙼𝚈 𝙲𝙾𝙼𝙼𝙰𝙽𝙳𝚂."""
     ABOUT_TXT = """✮ 𝙼𝚈 𝙽𝙰𝙼𝙴: {}
